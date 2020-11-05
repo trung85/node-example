c248782fd49e97a73d97a382bbbcde827d608cf5
Tade <phan.minh.trung@moneyforward.vn>
2020-11-05 09:26:53 +0700

    fix

--- server.js
+++ server.js
@@ -5,6 +5,37 @@ const express = require('express');
 // Constants
 const PORT = process.env.PORT || 3000;
 
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+const url = "https://media.gettyimages.com/photos/tan-dinh-church-with-pink-color-in-christmas-season-picture-id957612164?s=612x612"
+
+async function download() {
+  const response = await fetch(url);
+  const buffer = await response.buffer();
+  fs.writeFile(`/mnt/efs/image.jpg`, buffer, () =>
+    console.log('finished downloading!'));
+}
+
+download()
+
+
+let path = "/mnt/efs";
+
+fs.lstat(path, (err, stats) => {
+
+    if(err)
+        return console.log(err); //Handle error
+
+    console.log(`Is file: ${stats.isFile()}`);
+    console.log(`Is directory: ${stats.isDirectory()}`);
+    console.log(`Is symbolic link: ${stats.isSymbolicLink()}`);
+    console.log(`Is FIFO: ${stats.isFIFO()}`);
+    console.log(`Is socket: ${stats.isSocket()}`);
+    console.log(`Is character device: ${stats.isCharacterDevice()}`);
+    console.log(`Is block device: ${stats.isBlockDevice()}`);
+});
+
 // App
 const app = express();
 app.get('/', function (req, res) {
