5f7edb2139b62ac9e46b7d316861da4083be4ed7
Tade <phan.minh.trung@moneyforward.vn>
2021-01-07 16:03:17 +0700

    p3334

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 
 // Constants
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3334;
 
 const fs = require('fs');
 const fetch = require('node-fetch');
